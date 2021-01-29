e57fc7d7e90920fb60eb5085fc05849e69338f13
wilson <wilson.sfj@outlook.com>
2021-01-29 10:33:21 -0300

    criando funcoes serverless

--- handler.py
+++ handler.py
@@ -1,19 +1,16 @@
 from twitter import Twitter
+import json
 
-if __name__ == "__main__":
+
+def main(event, context):
     t = Twitter()
-    print('Loading followers..')
-        
+
     followers = t.follower()
     friends = t.following()
-    
-    print(f'Found {len(followers)} followers and found {len(friends)} following')
 
     follower_dict, friend_dict = t.followers_following_dict(followers, friends)
     non_friends = t.users_who_no_following(friends, follower_dict)
 
-    print(f"{len(non_friends)} users who don't follow you")
-
     t.unfollow_users(non_friends)
 
-    print('done')
+    return {'message': 'okay!'}
